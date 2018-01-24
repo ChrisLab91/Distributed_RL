7119a3b995748493bcfee311fec68b136eb42514
ChrisLab91 <christianholland91@gmail.com>
2018-01-25 00:34:11 +0100

    added some Bash scripts for vm testing

---
+++ ServerStarter/COPYFILES.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+scp -r ~/A3C/MyDistTest/Distributed_VM_example/ ubuntu@10.155.209.27:~
+scp -r ~/A3C/MyDistTest/Distributed_VM_example/ ubuntu@10.155.208.213:~
+scp -r ~/A3C/MyDistTest/Distributed_VM_example/ ubuntu@10.155.208.240:~
+scp -r ~/A3C/MyDistTest/Distributed_VM_example/ ubuntu@10.155.208.224:~
